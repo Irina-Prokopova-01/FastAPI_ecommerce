efca5fda55dc6207cb10d64448d7e7cb642c9898
Irina Prokopova <irina-prokopova.style@yandex.ru>
2025-06-15 15:41:43 +0300

    Синхронная работа команды Alembic для определения и создания экземпляра движка SQLAlchemy, подключения к этому движку и запуска транзакции, а также для правильного вызова движка миграции при запуске команды Alembic. Но так как мы хотим работать с асинхронным драйвером, который будет использовать асинхронные запросы, мы удалили файл alembic.ini и всю папку migrations и создаем среду миграции с помощью -t async для ассинхронной поддержки и меняя настройки создаем и выполняем миграции

--- app/backend/db.py
+++ app/backend/db.py
@@ -1,9 +1,20 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker, DeclarativeBase
+#
+#
+# engine = create_engine('sqlite:///ecommerce.db', echo=True)
+# SessionLocal = sessionmaker(bind=engine)
+#
+#
+# class Base(DeclarativeBase):
+#     pass
+
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
 
 
-engine = create_engine('sqlite:///ecommerce.db', echo=True)
-SessionLocal = sessionmaker(bind=engine)
+engine = create_async_engine('postgresql+asyncpg://postgres:1984)(zxZX@localhost:5432/ecommercee', echo=True)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 
 class Base(DeclarativeBase):
